b7ee30b30e13670a2449e0c5b7cb2825df9fb64c
Richard <richard@ganguo.hk>
2017-01-07 16:06:50 +0800

    Initial Commit

---
+++ RLCellModelProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RLCellModelProject
+//
+//  Created by Richard Liang on 2017/1/7.
+//  Copyright © 2017年 lwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RLCellModelProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RLCellModelProject
+//
+//  Created by Richard Liang on 2017/1/7.
+//  Copyright © 2017年 lwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
